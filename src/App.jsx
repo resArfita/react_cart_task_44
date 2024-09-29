7d843c779129df34e4d9a699af2f27d969256fc0
res Arfita <hresaarfita@gmail.com>
2024-09-30 01:47:07 +0700

    define state 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 //import './App.css'
 import Navbar from "./components/Navbar"
 
 function App() {
+  //State jumlah kuantitas Item
+  const [quantity, setQuantity] = useState(5)
+
+  
 
   return (
     <>
-      <Navbar />
+      <Navbar size={quantity} />
     </>
   )
 }
