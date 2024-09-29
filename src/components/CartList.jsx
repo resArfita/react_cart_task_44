9fd70c4a9dea9f927df3450f58c89e51218b718d
res Arfita <hresaarfita@gmail.com>
2024-09-30 02:00:12 +0700

    Menambahkan CarList component

---
+++ src/components/CartList.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+const CartList = () => {
+
+    return (
+        <>
+        <div className="">
+            <div className="grid grid-cols-4 ml-10 gap-2 pt-5">
+                <p className="underline decoration-violet-800 ml-5">Item</p>
+                <p className="underline decoration-violet-800 ml-6">Harga</p>
+                <p className="underline decoration-violet-800 ml-4">Kuantitas</p>
+                <p className="underline decoration-violet-800">H. Total</p>
+            </div>
+            
+        </div>
+        
+        </>
+    )
+}
+
+export default CartList
